e456e60ff5c163b19dafbff3ad4e31fe853a0cdd
tsw9114@126.com <tsw9114@126.com>
2015-12-05 15:21:17 +0800

    changes

--- 1.js
+++
@@ -1,6 +0,0 @@
-    $("#as ul li").hover(function(){
-            var index=$(this).index();
-			$(this).addClass("first").siblings().removeClass("first");		
-			$("#box div.img ul li").eq(index).css({"display":"block","opacity":"0"}).animate({"opacity":1},500).siblings().css("display","none");
-		});
-   
